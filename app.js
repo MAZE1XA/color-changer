fc4310c12b57c4174e6d9448b35cbc3c85a275a8
MAZE1XA <90650931+MAZE1XA@users.noreply.github.com>
2022-01-28 15:06:38 -0700

    added more coding

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const btn =  document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click",function () { 
-const randomnumber =getRandomNumber ();
+const randomNumber = getRandomNumber ();
 console.log(randomNumber);
+document.body.style.backgroundColor = colors[randomNumber]; 
+color.textContent = colors[randomNumber];
 });
-
 function getRandomNumber() { 
     return   Math.floor(Math.random()  * colors.length);   
 }
